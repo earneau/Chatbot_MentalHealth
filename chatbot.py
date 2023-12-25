c883ef56d59bb18da9c0e3ed42420d0cd97007bc
earneau <evan.arn@icloud.com>
2023-12-25 21:05:12 +0100

    Working chatbot generating answers understanding context within the user input

--- chatbot.py
+++ chatbot.py
@@ -1,57 +1,51 @@
-import requests
-from bs4 import BeautifulSoup
-from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.stem import WordNetLemmatizer
-import random
-import re
-import string
+from emotion import Emotion
 import pandas as pd
+import os
+import openai
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-df_test = pd.read_csv("archive/test.txt")
-df_train = pd.read_csv("archive/train.txt")
-df_val = pd.read_csv("archive/val.txt")
+openai.api_key = "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"
 
-def process_dataset(df):
-    if (df):
-        df.apply()
-    else:
-        print("slay")
+ # openai api token
+emotion = Emotion() # loading emotion detector
 
-def process_document(url):
-    # Send a GET request to the URL
-    response = requests.get(url)
+# loading and cleaning datasets
+train_df = pd.read_csv('archive/train.txt', names=['text', 'emotion'], sep=';')
+test_df = pd.read_csv('archive/test.txt', names=['text', 'emotion'], sep=';')
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the HTML content using BeautifulSoup with 'lxml' as the parser
-        soup = BeautifulSoup(response.content, 'lxml')
+train_df = emotion.process_data(train_df)
+test_df = emotion.process_data(test_df)
 
-        # Get all paragraphs using find_all('p')
-        paragraphs = soup.find_all('p')
+predictor = emotion.model(train_df,test_df) # training model
 
-        # Extract lowercase text from each paragraph and remove tags, symbols, and digits
-        paragraphs_text = [re.sub(r'<[^>]+>', ' ', re.sub(r'[^a-zA-Z\s]', ' ', paragraph.get_text().lower())) for paragraph in paragraphs]
+# function to generate a response based on user input and the detected emotion
+def generate_gpt3_response(user_input, detected_emotion):
+    # Prepend the detected emotion to the user input
+    input_text = f"User is feeling {detected_emotion}. {user_input}"
 
-        # Tokenize words
-        tokenized_words = [word_tokenize(sentence) for sentence in paragraphs_text]
-
-        # Lemmatize words and remove punctuation
-        lemmatizer = WordNetLemmatizer()
-        lemmatized_words = [
-            [lemmatizer.lemmatize(word) for word in words if word.isalnum()]  # Remove punctuation
-            for words in tokenized_words
+    completion = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+            {"role": "user", "content": input_text}
         ]
+    )
+    
+    try:
+        input_text = completion['choices'][0]['message']['content']
+        if input_text is not None:
+            return input_text
+        else:
+            return "Failed to generate response"
+    except KeyError:
+        return "Failed to extract input text from OpenAI response"
 
-        return lemmatized_words
+# Example usage
+user_input = input("You: ")
 
-    else:
-        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
-        return None
-
-# Example usage:
-hiv_who = "https://www.who.int/news-room/fact-sheets/detail/hiv-aids?gclid=Cj0KCQiAyeWrBhDDARIsAGP1mWSx7WbkfT1KFTbF2Av9exhL647vUeh4Wa-DwHtqcwi9M20jveQ4SGwaAlOqEALw_wcB"
-hiv_wiki = "https://en.wikipedia.org/wiki/HIV"
-hiv_cdc = "https://www.cdc.gov/hiv/basics/whatishiv.html"
-processed_hiv_who = process_document(hiv_who)
-processed_hiv_wiki = process_document(hiv_wiki)
-processed_hiv_cdc = process_document(hiv_cdc)
+while user_input not in ['exit', 'quit']:
+    detected_emotion = emotion.emotion_detection(predictor, user_input)  # Replace with your emotion detection logic
+    response = generate_gpt3_response(user_input, detected_emotion)
+    print("Bot:", response)
+    user_input = input("You: ")
+    
+    
